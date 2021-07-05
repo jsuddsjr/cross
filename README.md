736f9ab9a9e2e34ad3b7c066ff4e16723c8994e2
John Sudds <jsudds@microsoft.com>
2021-07-05 10:21:37 -0700

    Added files.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cross
+# cross (a work in progress)
+
+A simple implementation of a NYT-style crossword generator.
+
+WDD330 final project.
---
+++ js/__tests__/WordModel.tests.js
@@ -0,0 +1,7 @@
+import WordModel from "../WordModel";
+
+describe("WordModel", () => {
+  it("should ", () => {
+    // TODO: write tests.
+  });
+});
